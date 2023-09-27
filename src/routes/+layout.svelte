945947748ca0a7475d5138280aae1f0f0fb0042b
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-09-27 18:21:19 +0000

    Mock up of the UI for the projects page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,50 @@
 <script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
+
+	import {
+		AppShell,
+		AppBar,
+		Avatar,
+		Toast,
+		Modal,
+		type PopupSettings,
+		popup,
+		LightSwitch
+	} from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar
+			shadow="shadow-md"
+			padding="p-3 pl-5 pr-5 sm:pl-16 sm:pr-16 md:pl-20 md:pr-20 2xl:pl-36 2xl:pr-36"
+		>
+			<svelte:fragment slot="lead">
+				<a
+					href="/accounts"
+					class="select-none text-3xl"
+					style="font-family: 'Permanent Marker', cursive;"
+				>
+					USER NAME
+				</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface hidden sm:block"
+					href="/"
+					rel="noreferrer">Portfolio</a
+				>
+				<a class="btn btn-sm variant-ghost-surface hidden sm:block" href="/about" rel="noreferrer"
+					>About me</a
+				>
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<div class="m-5 sm:ml-16 sm:mr-16 sm:mt-10 md:ml-40 md:mr-40 2xl:ml-64 2xl:mr-64">
+		<slot />
+	</div>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,50 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import IconArrowTopRightOnSquare from '$lib/icons/IconArrowTopRightOnSquare.svelte';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	const projects = [
+		{
+			id: 1,
+			title: 'Project one with a long name',
+			body: 'On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, On project one i worked on one thing, ',
+			link: 'https://github.com/FrederikNorlyk',
+			linkText: 'Visit my github',
+			image: 'https://frederiknorlyk.dk/images/svelte.jpg',
+			technologies: ['Java', 'Flutter', 'Ext JS']
+		},
+		{
+			id: 2,
+			title: 'Project two',
+			body: 'On project two i worked on two things',
+			link: 'https://github.com/FrederikNorlyk',
+			linkText: 'Check out project two',
+			image: 'https://frederiknorlyk.dk/images/opinionMining.png',
+			technologies: ['Svelte', 'React', 'Python', 'TypeScript']
+		}
+	];
+</script>
+
+<div class="space-y-10">
+	{#each projects as project (project.id)}
+		<div class="space-y-1">
+			<div class="card rounded-md bg-white flex overflow-hidden items-center space-x-5">
+				<img class="w-32 border-r" src={project.image} alt="Thumbnail for {project.title}" />
+				<p class="text-3xl">{project.title}</p>
+			</div>
+			<div class="card rounded-md bg-white p-10 space-y-2">
+				<p>{project.body}</p>
+				{#if project.link}
+					<a class="chip variant-soft hover:variant-filled" href={project.link}>
+						<span><IconArrowTopRightOnSquare cssClass="h-3" /></span>
+						<span>{project.linkText}</span>
+					</a>
+				{/if}
+				<h2 class="text-sm">Technologies</h2>
+				<div class="space-x-1">
+					{#each project.technologies as technology (technology)}
+						<span class="badge variant-filled">{technology}</span>
+					{/each}
+				</div>
+			</div>
+		</div>
+	{/each}
 </div>
