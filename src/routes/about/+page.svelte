9ed859887055d7e87f9218aa91fe6f0ba6676716
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-09-28 15:42:38 +0000

    Fixed responsiveness issue

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,12 +3,9 @@
 	import LinkChip from '$lib/components/LinkChip.svelte';
 </script>
 
-<section class="card bg-white p-6 flex space-x-4">
-    <img class="w-48 rounded-full" src="/profile.png" alt={PUBLIC_USERNAME}>
-    <div>
-
-        <h1>Om mig</h1>
-        <p>{PUBLIC_ABOUT}</p>
-        <LinkChip href={PUBLIC_CONTACT} text="Kontakt mig" />
-    </div>
+<section class="card bg-white p-6 space-y-3">
+    <img class="w-48 h-48 rounded-full" src="/profile.png" alt={PUBLIC_USERNAME}>
+    <h1 class="text-xl">Om mig</h1>
+    <p>{PUBLIC_ABOUT}</p>
+    <LinkChip href={PUBLIC_CONTACT} text="Kontakt mig" />
 </section>
